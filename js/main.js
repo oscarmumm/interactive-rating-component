b692c6e518cd7c32c5ce92218a193b0e1ffbe8cf
Oscar Mumm <oscarmumm@gmail.com>
2023-07-27 16:45:38 -0300

    rating component working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const btnSubmit = document.getElementById('btn-submit');
 const submitCard = document.getElementById('submit-card');
 const thankyouCard = document.getElementById('thankyou-card');
+const ratingSpan = document.getElementById('rating-span');
+
 
 btnSubmit.addEventListener( 'click', (e) => {
+  e.preventDefault();
   submitCard.classList.toggle('d-none');
   thankyouCard.classList.toggle('d-none');
   thankyouCard.classList.toggle('flex-center');
+  let rating = document.querySelector('input[type="radio"]:checked');
+  ratingSpan.innerText = rating.value;
 })
